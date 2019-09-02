01756c6f3c111441fbc80ffd17b437bc0c4c75e4
Volker Bergner <volker@bergner.one>
2019-09-02 15:15:07 +0100

    Sinatra and shotgun testing

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org' do
 
 gem 'rspec'
 gem 'sinatra'
+gem 'shotgun'
 
 end
--- app.rb
+++ app.rb
@@ -1,1 +1,24 @@
 require 'sinatra'
+require 'shotgun'
+
+set    :session_secret, "here be dragons"
+
+get '/' do
+  'Hello world!'
+end
+
+  get '/secret' do
+    'Video killed the radio star'
+end
+
+get '/test' do
+  'test'
+end
+
+get '/test2' do
+  'test2'
+end
+
+  get '/test3' do
+    'test4'
+end
