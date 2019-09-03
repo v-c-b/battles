c5d23722f4c64559266dd8b7d23c576bb37687a6
Volker Bergner <volker@bergner.one>
2019-09-03 11:52:34 +0100

    Built cat picture reponse to /cat

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ get '/' do
   'Hello world!'
 end
 
-  get '/secret' do
-    'Video killed the radio star'
+get '/secret' do
+  'Video killed the radio star'
 end
 
 get '/test' do
@@ -19,6 +19,12 @@ get '/test2' do
   'test2'
 end
 
-  get '/test3' do
-    'test4'
+get '/test3' do
+  'test4'
 end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+      <img src='http://bit.ly/1eze8aE'>
+     </div>"
+  end
